9ece7f7d5593f76092dcf9b06471a634f9034eed
samo1228 <57116551+samo1228@users.noreply.github.com>
2019-11-20 10:11:46 +0000

    Update Poker.cpp
    Changes to poker.cpp to calculate the hand

--- Poker/Poker/Poker/Poker.cpp
+++ Poker/Poker/Poker/Poker.cpp
@@ -1,16 +1,29 @@
 #include "dealer.h"
+#include <iostream>
 
 int main()
 {
+	bool exit = false;
+	std::string exit_game_string;
 	DEALER* dealer = new DEALER();
-	dealer->draw( 5 );
 	dealer->swap();
-
-
+	dealer->draw( 5 );
+	
 
 	//dealer->draw( 5 );
 	//dealer->shuffle();
 	//dealer->draw( 5 );
+	while ( !exit )
+	{
+		std::cout << "Press enter to play";
+		std::getline( std::cin, exit_game_string );
 
+		if ( exit_game_string == "exit" )
+		{
+			exit = true;
+		}
+		dealer->swap();
+		dealer->draw( 5 );
+	}
 	delete dealer;
 }
