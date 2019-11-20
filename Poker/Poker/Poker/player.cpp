ba467cbc286cc0d42f469150f38f7c967a63f702
samo1228 <57116551+samo1228@users.noreply.github.com>
2019-11-20 10:16:52 +0000

    Update player.cpp
    Started adding the calculations for the different types of hands (so far have one pair, two pair, three of a kind and full house)

--- Poker/Poker/Poker/player.cpp
+++ Poker/Poker/Poker/player.cpp
@@ -1,6 +1,7 @@
 #include "dealer.h"
 #include "card.h"
 #include "player.h"
+#include <iostream>
 
 PLAYER::PLAYER()
 {
@@ -28,9 +29,78 @@ void PLAYER::swap_card( int card_to_swap, const CARD* card )
 	}
 }
 
-void PLAYER::calculate_hand()
+bool compare_name_func( const CARD* a, const CARD* b ) 
 {
+	return a->get_value() < b->get_value();
+}
 
+int PLAYER::calculate_hand()
+{
+	std::sort( hand.begin(), hand.end(), compare_name_func );
+	int best_hand = 0;
+	int first_pair_value = 0;
+	int second_pair_value = 0;
+	int first_triple_value = 0;
+
+	//triple
+	//check if there is a triple
+	for ( auto i = 0; i < hand.size() - 1; i++ )
+	{
+		if ( hand[i]->get_value() == hand[i + 1]->get_value() && (i + 2 < hand.size() && hand[i]->get_value() == hand[i + 2]->get_value()) )
+		{
+			best_hand = HAND_TYPE::THREE_OF_A_KIND;
+			first_triple_value = hand[i]->get_value();
+			break;
+		}
+	}
+	//Pair
+			//check if two cards match
+	for ( auto i = 0; i < hand.size() - 1; i++ )
+	{
+		if ( hand[i]->get_value() != first_triple_value && hand[i]->get_value() == hand[i+1]->get_value() &&  ( i + 2 >= hand.size() || hand[i]->get_value() != hand[i+2]->get_value() ) )
+		{
+			if ( best_hand == HAND_TYPE::THREE_OF_A_KIND )
+			{
+				best_hand = HAND_TYPE::FULL_HOUSE;
+			}
+			else
+			{
+				best_hand = HAND_TYPE::ONE_PAIR;
+			}
+			first_pair_value = hand[i]->get_value();
+
+			break;
+		}
+	}
+	// two pair
+			//check if two sets of two card match
+	if ( best_hand == HAND_TYPE::ONE_PAIR )
+	{
+		for ( auto i = 0; i < hand.size() - 1; i++ )
+		{
+			if ( hand[i]->get_value() != first_pair_value && hand[i]->get_value() == hand[i + 1]->get_value() && (i+2 >= hand.size() || hand[i]->get_value() != hand[i + 2]->get_value()) )
+			{
+				best_hand = HAND_TYPE::TWO_PAIR;
+				second_pair_value = hand[i]->get_value();
+				break;
+			}
+		}
+	}
+
+
+
+	std::cout << std::endl << "* Hand Value: *" << best_hand << std::endl;
+
+	//check if have five of a kind
+	   //loop through five cards check if all the same value
+	//check if have straight flush
+	    //look through all cards 
+			// - check if same suit
+			// check if numbers all together
+	//check if have 4 of a kind
+
+	//full house
+	return 0;
 }
 
 void PLAYER::print_hand()
