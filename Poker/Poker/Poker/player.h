e2b1b6ad9d83a6fb95f7bb7cecedadbc1ed05699
samo1228 <57116551+samo1228@users.noreply.github.com>
2019-11-20 10:14:42 +0000

    Update player.h
    Added an enum for the different possible hand types and created an array to order the cards in the hand to make it easier to calculate if the player had a winning hand

--- Poker/Poker/Poker/player.h
+++ Poker/Poker/Poker/player.h
@@ -1,21 +1,34 @@
 #pragma once
 #include "card.h"
+#include <array>
 
-const int PLAYER_HAND_SIZE = 5;
+constexpr int PLAYER_HAND_SIZE = 5;
 
+enum HAND_TYPE : int
+{
+	FIVE_OF_A_KIND = 1,
+	STRAIGHT_FLUSH = 2,
+	FOUR_OF_A_KIND = 4,
+	FULL_HOUSE = 8,
+	FLUSH = 16,
+	STRAIGHT = 32,
+	THREE_OF_A_KIND = 64,
+	TWO_PAIR = 128,
+	ONE_PAIR = 256,
+	HIGH_CARD = 512
+};
 class PLAYER
 {
 public:
 	PLAYER();
-	const CARD* hand[PLAYER_HAND_SIZE];
+
 
 	void print_hand();
 
 	void reset_hand();
 	void add_card( const CARD* card );
 	void swap_card( int card_to_swap, const CARD* card );
-	void calculate_hand();
+	int calculate_hand();
 private:
-
-	
+    std::array <const CARD*, PLAYER_HAND_SIZE> hand;
 };
