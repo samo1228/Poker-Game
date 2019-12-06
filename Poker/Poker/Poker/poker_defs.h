bc8f166806864e136517012e58090c5bfcdf45c9
samo1228 <57116551+samo1228@users.noreply.github.com>
2019-12-06 10:05:21 +0000

    Add files via upload

---
+++ Poker/Poker/Poker/poker_defs.h
@@ -0,0 +1,18 @@
+#pragma once
+
+constexpr int PLAYER_HAND_SIZE = 5;
+typedef std::array <const CARD*, PLAYER_HAND_SIZE> PLAYER_HAND;
+
+enum class HAND_TYPE : int
+{
+	HIGH_CARD,
+	PAIR,
+	TWO_PAIR,
+	THREE_OF_A_KIND,
+	STRAIGHT,
+	FLUSH,
+	FULL_HOUSE,
+	FOUR_OF_A_KIND,
+	STRAIGHT_FLUSH,
+	ROYAL_FLUSH
+};
