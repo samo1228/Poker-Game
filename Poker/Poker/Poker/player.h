aa161a000b6106dfd578de077ef9bfd11ea5ba04
samo1228 <57116551+samo1228@users.noreply.github.com>
2019-12-06 11:58:34 +0000

    Update player.h

--- Poker/Poker/Poker/player.h
+++ Poker/Poker/Poker/player.h
@@ -1,22 +1,10 @@
-#pragma once
-#include "card.h"
 #include <array>
 
-constexpr int PLAYER_HAND_SIZE = 5;
-
-enum HAND_TYPE : int
+class SAM
 {
-	FIVE_OF_A_KIND = 1,
-	STRAIGHT_FLUSH = 2,
-	FOUR_OF_A_KIND = 4,
-	FULL_HOUSE = 8,
-	FLUSH = 16,
-	STRAIGHT = 32,
-	THREE_OF_A_KIND = 64,
-	TWO_PAIR = 128,
-	ONE_PAIR = 256,
-	HIGH_CARD = 512
+
 };
+
 class PLAYER
 {
 public:
@@ -29,8 +17,9 @@ public:
 	void add_card( const CARD* card );
 	void swap_card( int card_to_swap, const CARD* card );
 	int calculate_hand();
+
 private:
-    std::array <const CARD*, PLAYER_HAND_SIZE> hand;
+    PLAYER_HAND hand;
 };
 
 namespace player_utilities
